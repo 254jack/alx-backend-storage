45939de4717a8a33c1f0991bb58d919a31046c51
254jack <taixonj@gmail.com>
2024-03-21 12:28:30 +0300

    glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,9 @@
--- Search bands with style Glam rock
--- Durantion current
-
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
+-- list all bands wtih Glam rock as their main style --
+SELECT band_name, 
+    CASE
+        WHEN split IS NULL THEN "Unknown"
+        ELSE 2022 - formed
+    END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
